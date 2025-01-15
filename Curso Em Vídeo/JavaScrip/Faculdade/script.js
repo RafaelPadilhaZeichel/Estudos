3bac96427d9a4d40c0cdc2c5d5ba2ea43e285d70
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-01-14 21:10:42 -0300

    Create script.js

---
+++ Curso Em Vídeo/JavaScrip/Faculdade/script.js
@@ -0,0 +1,18 @@
+/* Praticando javaScript*/
+
+/*
+* function nome(parametro){//Bloco de código que será executado}
+* let x = exemplo (2,3);
+*   function exemplo(a,b){
+*    return a+b;
+*   }
+*
+*
+*
+*/
+
+let x = exemplo (2,3);
+   function exemplo(a,b){
+    return a+b;
+   }
+   console.log(x)
