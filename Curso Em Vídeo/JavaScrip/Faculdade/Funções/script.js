a53580c5052ff5bdd5323cc28afc4d58e098b78d
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-01-14 21:14:53 -0300

    Js

---
+++ Curso Em Vídeo/JavaScrip/Faculdade/Funções/script.js
@@ -0,0 +1,26 @@
+/* Praticando javaScript*/
+
+/*
+* function nome(parametro){//Bloco de código que será executado}
+* let x = exemplo (2,3);
+*   function exemplo(a,b){
+*    return a+b;
+*   }
+*
+*
+*
+*/
+
+// Definindo outra função regular com dois parâmetros
+function soma(a,b){
+    return a+b;
+}
+
+// Chamando a função soma e armazenando o resultado em uma variável
+let resultado = soma(13,29);
+
+// Exibindo o resultado da função soma
+console.log("O resultado da soma é: "+ resultado);
+console.log("O resultado da soam é: "+ soma(13,29));
+
+//
