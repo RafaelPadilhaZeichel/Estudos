0e987196448e9efa314a473458a760d55b8e5bca
RafaelPadilhaZeichel <83683390+RafaelPadilhaZeichel@users.noreply.github.com>
2025-01-18 16:38:21 -0300

    Js

---
+++ Curso Em Vídeo/JavaScrip/Faculdade/Operadores/01-operadoresLogicos-e-tabelaVerdade.js
@@ -0,0 +1,19 @@
+// exemplos de operadoes logicos em JavaScript
+
+// Operador E lógico (&&)
+const a = true;
+const b = false;
+
+
+//console.log(`true && false: $fresultadoE1]"); // Saída: false
+//console.log(true && true: $(resultadoE2]); // Saída: true
+
+// Operador
+// Operador
+
+//console.log(true || false: $(resultadoOUl]); // Saida: true
+//console.log(false || false: $(resultadoOU2]); // Saida: false
+
+//console.log("|true: $(resultadoNao1)`); // Saida: false
+//console.log("!false: $(resultadoNao2)`); // Saida: true
+
