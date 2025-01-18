00c8aa38edd0c9c1d70b52e197d5d22d069fe5aa
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-01-17 22:27:04 -0300

    Create 02-operadores-aritmeticos.js

---
+++ Curso Em Vídeo/JavaScrip/Faculdade/Funções/02-operadores-aritmeticos.js
@@ -0,0 +1,18 @@
+// calculadora.js
+
+// Função que simula a operação de uma calculadora
+function calculadora(num1, num2) {
+
+    
+
+    // operação aritmetica basicas
+
+    // Incremento e decremento
+    // Vamos incrementear num1
+
+    // Vamos decrementar num2
+
+    // exibir oo resultados no console
+
+
+}
