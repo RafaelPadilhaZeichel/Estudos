e895e737ed9babdbe6634f51943a5c066a920684
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-01-22 21:54:12 -0300

    Update 000-teste.js

--- Faculdade/JavaScrip/000-teste.js
+++ Faculdade/JavaScrip/000-teste.js
@@ -5,3 +5,15 @@ let y = 3;
 let resultado = (x + y * 2) > (x % y) && x != y;
 
 console.log(resultado);
+
+let a = 10;
+
+let b = "10";
+
+let resultado2 = a == b;
+
+console.log(resultado2);
+
+let z = 10/0;
+
+console.log(z);
