52d4517566e80f3ee00bfbc263bd76debd39cc29
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-01-22 21:34:31 -0300

    Update 04-Operadores-de-atribuicao.js

---
+++ Faculdade/JavaScrip/04-Operadores-de-atribuicao.js
@@ -0,0 +1,35 @@
+// Exemplos de operadores de atribuição em jS
+
+// operador de atribuição báscio (=)
+
+/*function teste() {
+    if (a == 10) {
+        console.log("a é igual a 10")
+    } else {
+        console.log("a não é igual a 10")
+    }
+    return teste
+}*/
+
+let a = 10;
+let b = 5;
+console.log(`Valor de a: ${a}`)
+
+
+// operador de atribuição de adição (+=)
+//console.log(`Após a += 5, valor de a: ${a}`)
+
+// operador de atribuição de subtração (-=)
+//console.log(`Após a -= 3, valor de a: ${a}`)
+
+// operador de atribuição de multiplicação (*=)
+//console.log(`Após a *= 2, valor de a: ${a}`)
+
+// operador de atribuição de divisão (/=)
+//console.log(`Após a /= 4, valor de a: ${a}`)
+
+// operador de atribuição de módulo (%=)
+//console.log(`Após a %= 4, valor de a: ${a}`)
+
+// operador de atribuição de exponenciação (**=)
+//console.log(`Após a **= 5, valor de a: ${a}`)
