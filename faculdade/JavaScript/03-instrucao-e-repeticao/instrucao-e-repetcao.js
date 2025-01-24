3b25b2b6676b0b5eee7f3f956617eff3fdd78a16
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-01-23 21:39:37 -0300

    Update instrucao-e-repetcao.js

---
+++ faculdade/JavaScript/03-instrucao-e-repeticao/instrucao-e-repetcao.js
@@ -0,0 +1,12 @@
+// Varável que representa a previsão do tempo
+// pode ser "rainy" (Chuvoso), "sunny" (Ensolarado) ou "cloudy" (Nublado)
+
+let weatherForecast = "rainy";
+
+// Analogia com a vida real: decidindo se devemos levar um guarda-chuva
+
+if (weatherForecast === "rainy") {
+    console.log("É melhor levar um guarda-chuva hoje!");
+} else {
+    console.log("Não é necessário levar um guarda-chuva hoje!");
+}
