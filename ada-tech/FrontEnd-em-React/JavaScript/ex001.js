929312ee59315f330b538bf537a3dc3fe0043433
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-01-29 18:40:35 -0300

    JavaScript

---
+++ ada-tech/FrontEnd-em-React/JavaScript/ex001.js
@@ -0,0 +1,8 @@
+// ! Não utilize Var em seus códigos JavaScript, utilize Let ou Const
+
+let notaDoAluno = 10;
+const mediaDoAluno = 8;
+
+notaDoAluno = 9; // Na variavel Let conseguimos alterar o valor, já em uma variavel const não podemos mudar o seu valor, ele será fixo.
+
+console.log(notaDoAluno);
---
+++ ada-tech/FrontEnd-em-React/JavaScript/ex002.js
@@ -0,0 +1,12 @@
+const nota1 = 10;
+const nota2 = 8;
+
+const somaDasNotas = nota1 + nota2
+let mediaDasNotas = somaDasNotas / 2;
+
+console.log("Soma das Notas =",mediaDasNotas);
+
+const numero = 16;
+const raizQuadrada = Math.sqrt(numero);
+
+console.log("Raiz Quadrada de",numero,"=",raizQuadrada);
