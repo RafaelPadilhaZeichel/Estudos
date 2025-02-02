629332b05bbb2456c1a62b24ac0c67ec4b679a17
RafaelPadilhaZeichel <83683390+RafaelPadilhaZeichel@users.noreply.github.com>
2025-02-02 19:14:04 -0300

    Update for.js

---
+++ ada-tech/FrontEnd-em-React/JavaScript/Aulas 9/for.js
@@ -0,0 +1,31 @@
+const prompt = require("readline-sync") 
+
+// Expressão 1: Declara a váriavel
+// Expressão 2: Condição de permanência do for
+// Expressão 3: o incremento da várivavel de controle
+
+// for (expressão 1; expressão 2; expressão 3;){
+//    o escopo for
+// }
+
+for (let i = 0; i < 5; i++){
+    console.log(i)
+}
+
+// Limpando o console
+console.clear();
+
+// Exemplo
+
+let maiorNumero = 0;
+let numeroInformado;
+
+for(let i = 0; i < 5; i++){
+    numeroInformado = Number(prompt.question("Informe um número positivo: "))
+
+    if(numeroInformado > maiorNumero){
+        maiorNumero = numeroInformado
+    }
+}
+
+console.log("O maior número informado foi:",maiorNumero)
