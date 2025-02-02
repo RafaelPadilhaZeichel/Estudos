4cd412f350505abfd95c4abc00e819edcdaa8241
RafaelPadilhaZeichel <83683390+RafaelPadilhaZeichel@users.noreply.github.com>
2025-02-02 10:16:05 -0300

    Laços de repetição

---
+++ faculdade/JavaScript/03-instrucao-e-repeticao/While.js
@@ -0,0 +1,13 @@
+let quantidadeDeFlexoes = 0;
+
+let cansaco = false;
+
+while (!cansaco) {
+    quantidadeDeFlexoes++;
+    console.log("Eu fiz", quantidadeDeFlexoes, "Felxões!")
+
+    if (quantidadeDeFlexoes === 10){
+        cansaco = true;
+    }
+
+}
