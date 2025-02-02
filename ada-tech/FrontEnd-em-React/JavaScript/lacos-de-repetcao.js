efc6c31a95ccb68bd51db27c505d3fa0a77446ed
RafaelPadilhaZeichel <83683390+RafaelPadilhaZeichel@users.noreply.github.com>
2025-02-02 09:36:23 -0300

    Update lacos-de-repetcao.js

--- ada-tech/FrontEnd-em-React/JavaScript/lacos-de-repetcao.js
+++ ada-tech/FrontEnd-em-React/JavaScript/lacos-de-repetcao.js
@@ -9,15 +9,37 @@ const prompt = require("readline-sync")
 //}
 //console.log(saldo)
 
-let notaDoAluno = Number(prompt.question("Digite quanto você tirou na prova para eu fazer uma média da turma: "));
-let acumulador = 0;
-let contador = 0;
+//let notaDoAluno = Number(prompt.question("Digite quanto você tirou na prova para eu fazer uma média da turma: "));
+//let acumulador = 0;
+//let contador = 0;
 
-while (notaDoAluno >= 0){
-    acumulador = acumulador + notaDoAluno;
-    notaDoAluno = Number(prompt.question("Informe a próxima nota: "));
-    contador = contador + 1;
+//while (notaDoAluno >= 0){
+//    acumulador = acumulador + notaDoAluno;
+//    notaDoAluno = Number(prompt.question("Informe a próxima nota: "));
+//    contador = contador + 1;
+//}
+//let mediaDasNotas = acumulador / contador
+
+//console.log("A média das notas informadas eh:",mediaDasNotas);
+
+
+
+// * Exemplo 3
+
+// Math.random() => Entre 0 e 1 
+
+const numeroAleatorio = parseInt(Math.random() * 10)
+
+let numeroDoUsuario = Number(prompt.question("Informe um número entre 0 e 10: "))
+
+let tentativasDoUsuario = 1;
+
+while (numeroAleatorio !== numeroDoUsuario){
+    console.log("Você errou o número!")
+    numeroDoUsuario = Number(prompt.question("Informe um número entre 0 e 10: "))
+    tentativasDoUsuario += 1
 }
-let mediaDasNotas = acumulador / contador
 
-console.log("A média das notas informadas eh:",mediaDasNotas);
+console.log("Parabéns! Você acertou, o número era", numeroAleatorio, "Sua quantidade de tentativas foi", tentativasDoUsuario)
+
+
