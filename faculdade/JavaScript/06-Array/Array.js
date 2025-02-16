082d82dd2b6646b89acc6ae453397fe320068562
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-02-15 22:43:44 -0300

    Update Array.js

--- faculdade/JavaScript/06-Array/Array.js
+++ faculdade/JavaScript/06-Array/Array.js
@@ -1,24 +1,26 @@
-let array1 = []; 
+let array1 = [];
 let array2 = new Array();
-let array3 = [ 'Maça' , 'Banana' , 'Uva' ]; 
+let array3 = ["Maça", "Banana", "Uva"];
 
-console.log('Array 2', array1)
-console.log('Array 2', array2)
-console.log('Array 3', array3)
+console.log("Array 2", array1);
+console.log("Array 2", array2);
+console.log("Array 3", array3);
 
 // array1 = ['Teste1'];
 // array2 = ['Teste2'];
 // array3 = ['Teste3'];
 
-console.log('\n Array 1', array1)
-console.log('   Array 2', array2)
-console.log('   Array 3', array3)
+console.log("\n Array 1", array1);
+console.log("   Array 2", array2);
+console.log("   Array 3", array3);
 
-console.log('\n Array após adicionar elementos:', array1)
-array1.push('ele1')
-array2.push('ele2')
-array3.push('ele3')
+// A forma correta de adicionar elementos no Array é usando metódos espécificos para isso, como o .push();
 
-console.log('\n Novo Array 1', array1)
-console.log('   Novo Array 2', array2)
-console.log('   Novo Array 3', array3)
+console.log("\n Array após adicionar elementos:", array1);
+array1.push("ele1");
+array2.push("ele2");
+array3.push("ele3");
+
+console.log("\n Novo Array 1", array1);
+console.log("   Novo Array 2", array2);
+console.log("   Novo Array 3", array3);
