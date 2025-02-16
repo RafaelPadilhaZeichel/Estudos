29f5a523ba3779e4a2797cdaad86a525cfc6ff2f
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-02-15 22:37:09 -0300

    Create Array.js

---
+++ faculdade/JavaScript/06-Array/Array.js
@@ -0,0 +1,15 @@
+let arrayVazio1 = []; 
+let arrayVazio2 = new Array();
+let arrayCheio3 = [ 'Maça' , 'Banana' , 'Uva' ]; 
+
+console.log('Array 2', arrayVazio1)
+console.log('Array 2', arrayVazio2)
+console.log('Array 3', arrayCheio3)
+
+arrayVazio1 = ['Teste1'];
+arrayVazio2 = ['Teste2'];
+arrayCheio3 = ['Teste3'];
+
+console.log('\nArray 1', arrayVazio1)
+console.log('Array 2', arrayVazio2)
+console.log('Array 3', arrayCheio3)
