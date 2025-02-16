f752b17d84de9c2dd300a9445d6e4597cd66966e
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-02-15 22:52:20 -0300

    Update Array.js

--- faculdade/JavaScript/06-Array/Array.js
+++ faculdade/JavaScript/06-Array/Array.js
@@ -14,7 +14,7 @@ console.log("\n Array 1", array1);
 console.log("   Array 2", array2);
 console.log("   Array 3", array3);
 
-// A forma correta de adicionar elementos no Array é usando metódos espécificos para isso, como o .push();
+//! A forma correta de adicionar elementos no Array é usando metódos espécificos para isso, como o .push();
 
 console.log("\n Array após adicionar elementos:", array1);
 array1.push("ele1");
@@ -24,3 +24,9 @@ array3.push("ele3");
 console.log("\n Novo Array 1", array1);
 console.log("   Novo Array 2", array2);
 console.log("   Novo Array 3", array3);
+
+//! Forma de acessar um elemento no meu array.
+
+let primeiroElemento = array1[0];
+console.log('\n Primeiro elemento:', primeiroElemento);
+
