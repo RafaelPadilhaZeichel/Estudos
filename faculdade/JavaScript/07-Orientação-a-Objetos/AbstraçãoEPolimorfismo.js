81202a88da2fc332ca7cd4c9a6224e0e065ba50c
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-02-25 18:31:24 -0300

    Update AbstraçãoEPolimorfismo.js

--- faculdade/JavaScript/07-Orientação-a-Objetos/AbstraçãoEPolimorfismo.js
+++ faculdade/JavaScript/07-Orientação-a-Objetos/AbstraçãoEPolimorfismo.js
@@ -1,28 +1,67 @@
 // Classe base 'Imovel'
 
 class Imovel {
-    constructor(endereco, tamanho){
-        this.endereco= endereco;
-        this.tamanho= tamanho;
+    constructor(endereco, tamanho) {
+      this.endereco = endereco;
+      this.tamanho = tamanho;
     }
-
+  
     // Método abstrato para descrever o imóvel (Deve ser implementado nas subclasses)
     descrever() {
-        throw new Error("Este método deve ser implementado por subclasses");
-        
+      throw new Error("Este método deve ser implementado por subclasses");
     }
-}
-
-// Classe derivada "Casa" que herda de "Imovel"
-class casa extends Imovel {
-    constructor(endereco, tamanho, cor, numeroDeQaurtos, temGaragem) {
-        super(endereco, tamanho);
-        this.cor = cor;
-        this.numeroDeQaurtos = numeroDeQaurtos;
-        this.temGaragem = temGaragem;
+  }
+  
+  // Classe derivada "Casa" que herda de "Imovel"
+  class Casa extends Imovel {
+    constructor(endereco, tamanho, cor, numeroDeQuartos, temGaragem) {
+      super(endereco, tamanho);
+      this.cor = cor;
+      this.numeroDeQuartos = numeroDeQuartos;
+      this.temGaragem = temGaragem;
     }
-
-    
-}
-
-    
+  
+    descrever() {
+      let descricao = `Casa localizada em ${this.endereco}, de cor ${this.cor}, com ${this.numeroDeQuartos} quartos, de tamanho ${this.tamanho}m²`;
+  
+      // Operador ternário
+      descricao += this.temGaragem
+        ? " e possui garagem."
+        : " e não possui garagem";
+  
+      return descricao;
+    }
+  }
+  
+  // Classe derivada "Apartamento" que herda de "Imovel"
+  class Apartamento extends Imovel {
+    constructor(endereco, tamanho, numeroDoAndar, possuiElevador) {
+      super(endereco, tamanho);
+      this.numeroDoAndar = numeroDoAndar;
+      this.possuiElevador = possuiElevador;
+    }
+  
+    // Implementando o método descrever
+    descrever() {
+      let descricao = `Apartamento localizado em ${this.endereco}, no ${this.numeroDoAndar}° andar, de tamanho ${this.tamanho}m²`;
+  
+      descricao += this.possuiElevador
+        ? " e possui elevador."
+        : " e não possui elevador";
+  
+      return descricao;
+    }
+  }
+  
+  // Função para descrever um imóvel (Polimorfismo)
+  function descreverImovel(Imovel) {
+    console.log(Imovel.descrever());
+  }
+  
+  // Criação de objetos (instâncias das classes)
+  const minhaCasa = new Casa("Rua A, 123", 120, "Azul", 3, true);
+  const meuApartamento = new Apartamento("Avenida B, 456", 85, 7, true);
+  
+  // Manipulação dos objetos e exibição das descrições usando polimorfismo
+  descreverImovel(minhaCasa);
+  descreverImovel(meuApartamento);
