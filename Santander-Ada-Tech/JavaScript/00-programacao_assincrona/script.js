8048b569249735aea72837608271c2b70604114b
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-03-06 20:54:27 -0300

    Programação assíncrona por callbacks

--- Santander-Ada-Tech/JavaScript/00-programacao_assincrona/script.js
+++ Santander-Ada-Tech/JavaScript/00-programacao_assincrona/script.js
@@ -1,7 +1,27 @@
 //! A programação assíncorna pode ser aplicada de três formas:
+const fs = require('fs'); // Importando o módulo fs
 
-//! 1. Por Callbacks -> 
+
+//* 1. Por Callbacks -> 
+// lendo um arquivo de forma assíncrona (Função assíncrona)
+// Callbacks são funções que são passadas como argumentos para outras funções
+// e são chamadas quando um evento ocorre ou quando uma tarefa é concluída.
+
+
+console.log('Antes da leitura do arquivo');
+
+fs.readFile("Santander-Ada-Tech/JavaScript/00-programacao_assincrona/arquivo.txt",(erro, conteudoDoArquivo) => {
+    if (erro) {
+        console.log('Erro ao ler o arquivo', erro);
+    } else {
+        console.log(conteudoDoArquivo.toString());
+    }
+});
+
+console.log('Depois da leitura do arquivo');
+
 
 //! 2. Por Promises -> 
 
 //! 3. Resolvendo Promises usando Async/Await ->
+
