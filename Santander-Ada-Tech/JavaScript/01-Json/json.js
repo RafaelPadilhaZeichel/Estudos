37b3705d0eca7cb029ea838f46509dbe5a3172c5
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-03-07 20:54:28 -0300

    Configurando o EsLint

--- Santander-Ada-Tech/JavaScript/01-Json/json.js
+++ Santander-Ada-Tech/JavaScript/01-Json/json.js
@@ -1,3 +1,7 @@
 // Json é uma formatação leve de troca de dados. Para seres humanos, é fácil de ler e escrever. Para máquinas, é fácil de interpretar e gerar. Está baseado em um subconjunto da linguagem de programação JavaScript, Standard ECMA-262 3ª Edição - Dezembro 1999. JSON é em formato texto e completamente independente de linguagem, pois usa convenções que são familiares às linguagens C e familiares, incluindo C++, C#, Java, JavaScript, Perl, Python e muitas outras. Estas propriedades fazem com que JSON seja um formato ideal de troca de dados.
 
+//Exemplo: configurando o Eslint, o eslint é uma configuração que faz com que o código fique mais limpo e organizado, para isso é necessário instalar o eslint e o prettier, e configurar o arquivo .eslintrc.json, para que o eslint funcione corretamente.
 
+const aula = "JSON";
+
+console.log(aula);
