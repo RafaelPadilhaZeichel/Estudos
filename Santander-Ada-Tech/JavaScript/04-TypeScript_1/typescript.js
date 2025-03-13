bfe3ea8204df4a946cdce9343e13fcd83d6f3d68
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-03-13 10:51:53 -0300

    União de tipos, literal types

--- Santander-Ada-Tech/JavaScript/04-TypeScript_1/typescript.js
+++ Santander-Ada-Tech/JavaScript/04-TypeScript_1/typescript.js
@@ -5,33 +5,81 @@
 // const numero4 = undefined
 // console.log(numero1 + numero2) // NaN, isso é uma operação inválida
 // console.log("Olá, mundo!")
-// // TypeScript é um superconjunto de JavaScript que adiciona tipagem estática ao código
-// // TypeScript é um conjunto de ferramentas que compila para JavaScript
+//! TypeScript é um superconjunto de JavaScript que adiciona tipagem estática ao código
+//! TypeScript é um conjunto de ferramentas que compila para JavaScript
 // function somar(a: number, b: number): number {
 //     return a + b
 // }
 // somar(1, 2) // 3
 // const nomeDoUsuario = prompt("Qual é o seu nome?")
 // console.log("Olá, " + nomeDoUsuario?.toLocaleUpperCase() + "!")
-// number[] ou Array<number> é um array de números
-// string[] ou Array<string> é um array de strings
-var numero = [1, 2, 3, 4, 5];
+//! number[] ou Array<number> é um array de números
+//! string[] ou Array<string> é um array de strings
+// const numero: Array<number> = [1, 2, 3, 4, 5]
 // const misto: (number | string)[] = [1, 2, 3, 4, 5, "seis", "sete", "oito"]
-numero.push(10);
-numero.push(53);
-numero.push(18);
-numero.push(15);
-numero.push(66);
-var menoresDeIdade = numero.filter(function (idade) { return idade < 18; });
-var maioresDeIdade = numero.filter(function (idade) { return idade >= 18; });
-console.log(menoresDeIdade);
-console.log(maioresDeIdade);
-//tuplas são arrays de tamanho fixo e com tipos fixos
-var pessoa = ["Alice", 25];
-var pessoa2 = {
-    nome: "Alice",
-    idade: 25,
-    profissao: "Desenvolvedora",
-    altura: 1.70
-};
-// type
+// numero.push(10)
+// numero.push(53)
+// numero.push(18)
+// numero.push(15)
+// numero.push(66)
+// const menoresDeIdade = numero.filter((idade) => idade < 18)
+// const maioresDeIdade = numero.filter((idade) => idade >= 18)
+// console.log(menoresDeIdade)
+// console.log(maioresDeIdade)
+//! tuplas são arrays de tamanho fixo e com tipos fixos
+// const pessoa: [string, number] = ["Alice", 25]
+//! tipagem de objetos em TypeScript é feita com interfaces ou types (tipos)
+//! Object types
+//! interface
+// interface Person {
+//     nome: string;
+//     idade: number;
+//     profissao: string;
+//     altura: number;
+// }
+// const pessoa2: Person = {
+//     nome: "Alice",
+//     idade: 25,
+//     profissao: "Desenvolvedora",
+//     altura: 1.70
+// }
+//! type
+// type Person = {
+//     nome: string;
+//     idade: number;
+//     profissao?: string;
+//     altura: number;
+// }
+// type Profissao = {
+//     nome: string;
+//     salario: number;
+//     cargo: string;
+//     curso?: string;
+// }
+// const emprego: Profissao = {
+//     nome: "Alice",
+//     salario: 5000,
+//     cargo: "Desenvolvedora",
+//     curso: "Engenharia de Software"
+// }
+// console.log(emprego)
+//! isso é um uniao de tipos e literal types, ou seja, um tipo que aceita um valor específico
+function chooseNumber(numero1, numero2, criterio // é um tipo que aceita apenas "greater" ou "smaller"
+) {
+    switch (criterio) {
+        case "greater":
+            return numero1 > numero2 ? numero1 : numero2;
+        case "smaller":
+            return numero1 < numero2 ? numero1 : numero2;
+        default:
+            var numeroAleatorio = Math.random();
+            if (numeroAleatorio > 0.5) {
+                return numero1;
+            }
+            else {
+                return numero2;
+            }
+    }
+}
+var numeroEscolhido = chooseNumber(10, 20, "greater");
+console.log(numeroEscolhido);
