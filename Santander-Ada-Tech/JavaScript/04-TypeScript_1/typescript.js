9e58f077872cf6dc42a62b933c1f7ba1bb05fda9
RafaelPadilhaZeichel <padilharafael17@gmail.com>
2025-03-12 21:26:59 -0300

    Estudando tipagem

--- Santander-Ada-Tech/JavaScript/04-TypeScript_1/typescript.js
+++ Santander-Ada-Tech/JavaScript/04-TypeScript_1/typescript.js
@@ -1,25 +1,15 @@
 // const numero1 = 4
 // const numero2 = "5"
-
 // console.log(numero1 + numero2) // 45, isso é uma concatenação de strings
-
 // const numero3 = 4
 // const numero4 = undefined
-
 // console.log(numero1 + numero2) // NaN, isso é uma operação inválida
-
 // console.log("Olá, mundo!")
 // // TypeScript é um superconjunto de JavaScript que adiciona tipagem estática ao código
 // // TypeScript é um conjunto de ferramentas que compila para JavaScript
-
-
 // function somar(a: number, b: number): number {
 //     return a + b
 // }
-
 // somar(1, 2) // 3
-
-
 // const nomeDoUsuario = prompt("Qual é o seu nome?")
 // console.log("Olá, " + nomeDoUsuario?.toLocaleUpperCase() + "!")
-
---
+++ Santander-Ada-Tech/JavaScript/05-TypeScript_2/Readme.md
@@ -0,0 +1,1 @@
+Foi continuado na pasta TypeScript 1
