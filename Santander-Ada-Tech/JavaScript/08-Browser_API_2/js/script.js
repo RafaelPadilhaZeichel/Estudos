bb0057ecfaed3de1c394aadfefacffabdb8f99b9
RafaelPadilhaZeichel <83683390+RafaelPadilhaZeichel@users.noreply.github.com>
2025-03-15 10:08:40 -0300

    Criação de páginas dinâmicas

---
+++ Santander-Ada-Tech/JavaScript/08-Browser_API_2/js/script.js
@@ -0,0 +1,58 @@
+const aumentarBotao = document.querySelector('#aumentar-botao')
+const diminuirBotao = document.querySelector('#diminuir-botao')
+
+const contadorElemento = document.querySelector('#contador')
+
+const input = document.querySelector('#input')
+
+aumentarBotao.addEventListener('click', () => {
+   const valorAtual = Number(contadorElemento.textContent)
+
+   contadorElemento.textContent = valorAtual + 1
+
+   aumentarBotao.classList.add('btn')
+   diminuirBotao.classList.remove('btn')
+})
+
+diminuirBotao.addEventListener('click', () => {
+    const valorAtual = Number(contadorElemento.textContent)
+ 
+    contadorElemento.textContent = valorAtual - 1
+
+    
+   diminuirBotao.classList.add('btn')
+   aumentarBotao.classList.remove('btn')
+ })
+
+input.addEventListener('input', () => {
+    console.log(input.value)
+})
+
+// Adicionando estilos inline no elemento contador
+
+contadorElemento.style.color = 'red';
+contadorElemento.style.padding = '0 2rem'
+
+// Mamnipular Classes
+
+console.log(diminuirBotao.classList);
+
+// Mudando o tema da página
+
+const themebutton = document.querySelector('#theme')
+
+let darkTheme = false;
+
+themebutton.addEventListener('click', () => {
+    darkTheme = !darkTheme  
+
+    const body = document.querySelector('body')
+
+    if (darkTheme) {
+        body.style.backgroundColor = 'black'
+        body.style.color = 'white'
+    } else {
+        body.style.backgroundColor = 'white'
+        body.style.color = 'black'
+    }
+})
